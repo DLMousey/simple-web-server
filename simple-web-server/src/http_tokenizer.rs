bae8b9514445af4e96c8dd244a395ae6d6798ce9
DLMousey <tom@deadlyllama.com>
2019-11-12 23:29:30 +0000

    Rename methods for clarity

--- simple-web-server/src/http_tokenizer.rs
+++ simple-web-server/src/http_tokenizer.rs
@@ -22,7 +22,7 @@ pub fn tokenize(req: Cow<'_, str>) -> HttpReq {
         // Because the method doesn't follow a Key: val structure, we'll count our blessings
         // that the spec defines it as the very first thing in the request and match it first.
         if index == 0 {
-            t.method = get_first_word(line_val);
+            t.method = get_key(line_val);
         }
 
         // All other values in the headers are following a Key: Val structure,
@@ -30,12 +30,12 @@ pub fn tokenize(req: Cow<'_, str>) -> HttpReq {
         // For each key we derive from the request headers, we'll run a match.
         // If the normalized_key is a match to any of the fields in the struct we'll set
         // the value of that field to the value we've derived from the request.
-        let normalized_key = get_first_word(line_val)
+        let normalized_key = get_key(line_val)
             .to_lowercase()
             .replace("-", "_")
             .replace(":", "");
 
-        let value = get_key_value(line_val);
+        let value = get_value(line_val);
 
         // @todo - Refactor into a less disgusting mess, couldn't get this working with `match`
         if normalized_key == String::from("host") {
@@ -50,14 +50,14 @@ pub fn tokenize(req: Cow<'_, str>) -> HttpReq {
     return t;
 }
 
-fn get_first_word(line: &str) -> String {
+fn get_key(line: &str) -> String {
     return line.split_whitespace()
         .next()
         .unwrap_or("")
         .to_string();
 }
 
-fn get_key_value(line: &str) -> String {
+fn get_value(line: &str) -> String {
     let mut split = line.split(": ");
 
     split.next(); // Move the iterator along once...
